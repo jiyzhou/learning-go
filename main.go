d3ad75ea8e5eb003872dca456017320aed51b2dc
Jiyi Zhou <jiyi.zhou@sendgrid.com>
2022-06-29 15:29:53 -0700

    round the fractional sum to two decimal places

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -14,15 +15,18 @@ func main() {
 	value1, _ := reader.ReadString('\n')
 	valueFloat1, err := strconv.ParseFloat(strings.TrimSpace(value1), 64)
 	if err != nil {
+		fmt.Println(err)
 		panic("Value 1 must be a number")
 	}
 	fmt.Print("Value 2: ")
 	value2, _ := reader.ReadString('\n')
 	valueFloat2, err := strconv.ParseFloat(strings.TrimSpace(value2), 64)
 	if err != nil {
+		fmt.Println(err)
 		panic("Value 2 must be a number")
 	}
 	result := valueFloat1 + valueFloat2
+	result = math.Round(result*100) / 100
 	fmt.Printf("The sum of %v and %v is %v\n", valueFloat1, valueFloat2, result)
 
 }
