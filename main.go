010de3624837462b0e34f6d3b825a8e86be96fd1
Jiyi Zhou <jiyi.zhou@sendgrid.com>
2022-06-29 15:25:31 -0700

    chapter 2 challenge

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Value 1: ")
+	value1, _ := reader.ReadString('\n')
+	valueFloat1, err := strconv.ParseFloat(strings.TrimSpace(value1), 64)
+	if err != nil {
+		panic("Value 1 must be a number")
+	}
+	fmt.Print("Value 2: ")
+	value2, _ := reader.ReadString('\n')
+	valueFloat2, err := strconv.ParseFloat(strings.TrimSpace(value2), 64)
+	if err != nil {
+		panic("Value 2 must be a number")
+	}
+	result := valueFloat1 + valueFloat2
+	fmt.Printf("The sum of %v and %v is %v\n", valueFloat1, valueFloat2, result)
+
+}
