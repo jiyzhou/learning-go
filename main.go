34a1afc48b32a3a0da8b094ae5e2d05ace7a9f19
Jiyi Zhou <jiyi.zhou@sendgrid.com>
2022-06-29 17:56:51 -0700

    create a more advanced calculator app)

--- main.go
+++ main.go
@@ -11,22 +11,64 @@ import (
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Value 1: ")
-	value1, _ := reader.ReadString('\n')
-	valueFloat1, err := strconv.ParseFloat(strings.TrimSpace(value1), 64)
-	if err != nil {
-		fmt.Println(err)
-		panic("Value 1 must be a number")
-	}
-	fmt.Print("Value 2: ")
-	value2, _ := reader.ReadString('\n')
-	valueFloat2, err := strconv.ParseFloat(strings.TrimSpace(value2), 64)
-	if err != nil {
-		fmt.Println(err)
-		panic("Value 2 must be a number")
-	}
-	result := valueFloat1 + valueFloat2
+	value1 := readOperand("1", reader)
+	value2 := readOperand("2", reader)
+	validOperators := []string{"+", "-", "*", "/"}
+	operator := readOperator(validOperators, reader)
+	result := calcuate(value1, value2, operator)
 	result = math.Round(result*100) / 100
-	fmt.Printf("The sum of %v and %v is %v\n", valueFloat1, valueFloat2, result)
+	fmt.Printf("The result is %v\n", result)
 
+}
+
+func calcuate(value1, value2 float64, operator string) float64 {
+	var result float64
+	switch operator {
+	case "+":
+		result = value1 + value2
+	case "-":
+		result = value1 - value2
+	case "*":
+		result = value1 * value2
+	case "/":
+		if value2 != 0 {
+			result = value1 / value2
+		} else {
+			panic("Divided by zero\n")
+		}
+	}
+	return result
+}
+
+func readOperand(i string, reader *bufio.Reader) float64 {
+	fmt.Printf("Value %s: ", i)
+	input, _ := reader.ReadString('\n')
+	inputFloat, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+	if err != nil {
+		fmt.Println(err)
+		panicStr := "Value " + i + " must be a number\n"
+		panic(panicStr)
+	}
+	return inputFloat
+}
+
+func readOperator(valid []string, reader *bufio.Reader) string {
+	fmt.Printf("Select an operation ( + - * / ): ")
+	input, _ := reader.ReadString('\n')
+	input = input[:len(input)-1]
+	strings.TrimSpace(input)
+	if !contains(valid, input) {
+		panicStr := input + " is not a valid operator\n"
+		panic(panicStr)
+	}
+	return input
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
